8dcd008c076577ac7f51d236f7be95a632cad379
yusuf çırak <yusufcirak1327@outlook.com>
2021-04-14 23:34:14 +0300

    Kaydetne Methodu yapıldı

--- VizeOdev/Form1.cs
+++ VizeOdev/Form1.cs
@@ -12,6 +12,10 @@ using System.IO;
 
 namespace VizeOdev
 {
+
+
+
+
     public partial class Form1 : Form
     {
         DataTable tablo = new DataTable();
@@ -19,20 +23,19 @@ namespace VizeOdev
         {
             InitializeComponent();
 
-            
+
 
         }
 
 
 
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+
             dataGridView1.DataSource = tablo;
             tablo.Columns.Add("Döviz Kodu", typeof(string));
             tablo.Columns.Add("Birim", typeof(float));
@@ -53,23 +56,74 @@ namespace VizeOdev
             string usd5 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='USD'] / ForexSelling").InnerXml;
             string usd6 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='USD'] / BanknoteBuying").InnerXml;
             string usd7 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='USD'] / BanknoteSelling").InnerXml;
-            
 
-            tablo.Rows.Add(usd,usd2,usd3,usd4,usd5,usd6,usd7); 
-           
+            string aud = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='AUD'] / Isim").InnerXml;
+            string aud2 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='AUD'] / Unit").InnerXml;
+            string aud3 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='AUD'] / CurrencyName").InnerXml;
+            string aud4 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='AUD'] / ForexBuying").InnerXml;
+            string aud5 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='AUD'] / ForexSelling").InnerXml;
+            string aud6 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='AUD'] / BanknoteBuying").InnerXml;
+            string aud7 = xmlDoc.SelectSingleNode("Tarih_Date / Currency[@Kod ='AUD'] / BanknoteSelling").InnerXml;
+
+            tablo.Rows.Add(usd, usd2, usd3, usd4, usd5, usd6, usd7);
+            tablo.Rows.Add(aud, aud2, aud3, aud4, aud5, aud6, aud7);
             //usd = tablo.Columns[0].ToString();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-          
-                   
 
-            
 
-           
-            
-            
+
+
+
+
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            txtAktar.txtKaydet(dataGridView1);
+
+        }
+
+
+
+        public class txtAktar
+        {
+            public static void txtKaydet(DataGridView veriTablosu)
+            {
+                SaveFileDialog dosyakaydet = new SaveFileDialog();
+                dosyakaydet.FileName = "Döviz Kurları";
+                dosyakaydet.InitialDirectory = Environment.SpecialFolder.Desktop.ToString();
+                dosyakaydet.Filter = "Txt Dosyası|*.txt";
+                if (dosyakaydet.ShowDialog() == DialogResult.OK)
+                {
+                    TextWriter txt = new StreamWriter(dosyakaydet.FileName);
+                    foreach (DataGridViewColumn srg in veriTablosu.Columns)
+                    {
+                        txt.Write(srg.HeaderText + ";");
+                    }
+                    txt.Write("\n");
+                    foreach (DataGridViewRow satir in veriTablosu.Rows)
+                    {
+                        foreach (DataGridViewCell hucre in satir.Cells)
+                        {
+                            //txt.Write(hucre.Value.ToString() + ";");
+                            txt.Write(hucre.Value + ";");
+
+                        }
+                        txt.Write("\n");
+
+
+                    }
+                    txt.Close();
+                    MessageBox.Show("TXT dosyası başarıyla oluşturuldu!\n" + "Dosya Konumu: " + dosyakaydet.FileName);
+                }
+
+            }
+
         }
     }
 }
